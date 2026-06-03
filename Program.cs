b4bf55d79dc7e21cd560c87a7e2d70741c20d3a7
SaiPriyaReddy <saipriyareddy530@gmail.com>
2026-06-03 18:37:16 +0530

    first exercise

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Reflection.Metadata;
+
+// Console.WriteLine("Hello, World!");
+
+// string name = "sai";
+// Console.WriteLine(name);
+
+//! exercise-1 create a program that accepts name and age and displays them
+
+// Console.WriteLine("Enter your name: ");
+// string name = Console.ReadLine();
+// Console.WriteLine("Enter your age: ");
+// int age = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("name:" + name);
+// Console.WriteLine( "age:" + age);
+
+//! exercise-2 check whether a number is Even or Odd
+
+// Console.WriteLine("Enter the number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if(number % 2 == 0)
+// {
+//     Console.WriteLine("It is an even number");
+// }
+// else
+// {
+//     Console.WriteLine("It is a odd number");
+// }
+
+
+//! exercise-3 print numbers from 1 to 20 using a loop
+
+// for(int i = 1; i<=20; i++)
+// {
+//     Console.WriteLine(i);
+// }
+
+
+//! exercise-4 store 5 employee names in a list and display them
+
+// List<string> employees = new List<string>();
+
+// employees.Add("Sai");
+// employees.Add("Priya");
+// employees.Add("Krishna");
+// employees.Add("Radha");
+// employees.Add("Shiva");
+
+// foreach(var employee in employees)
+// {
+//     Console.WriteLine(employee);
+// }
+
+
+//! Exercise-5 store product name and price in a Dictionary and display them
+
+// Dictionary<string , int> products = new Dictionary<string,int>();
+
+// products.Add("Headset" , 5000);
+// products.Add("Bag" , 1000);
+// products.Add("Monitor" , 45000);
+
+// foreach(var product in products)
+// {
+//     Console.WriteLine($"{product.Key}:{product.Value}");
+// }
